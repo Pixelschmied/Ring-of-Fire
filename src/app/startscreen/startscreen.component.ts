b11d0455b74f4cf60ff34e839e9d0a6f3573a5cb
Pixelschmied <66792737+Pixelschmied@users.noreply.github.com>
2024-09-07 16:04:02 +0200

    added routing for standalone component

--- src/app/startscreen/startscreen.component.ts
+++ src/app/startscreen/startscreen.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-startscreen',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './startscreen.component.html',
   styleUrl: './startscreen.component.scss'
 })
 export class StartscreenComponent {
+
+  constructor(private router: Router) { }
+
+  newGame() {
+    // Start Game
+    this.router.navigateByUrl('/game')
+  }
 
 }
