c2bb3b7221dbecc467b9d7cede0b3de527ca34fc
Pixel <66792737+Pixelschmied@users.noreply.github.com>
2024-10-09 17:55:56 +0200

    added player component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { StartscreenComponent } from './startscreen/startscreen.component';
 import { GameComponent } from './game/game.component';
+import { PlayerComponent } from './player/player.component';
 
 export const routes: Routes = [
     { path: '', component: StartscreenComponent},
-    { path: 'game', component: GameComponent}
+    { path: 'game', component: GameComponent},
+    { path: 'player', component: PlayerComponent}
 ];
