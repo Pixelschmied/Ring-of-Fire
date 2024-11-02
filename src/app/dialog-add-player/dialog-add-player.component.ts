2e2007ac28902c4622f1996816816f87aa4740cf
Pixelschmied <66792737+Pixelschmied@users.noreply.github.com>
2024-11-02 15:47:28 +0100

    Update dialog-add-player.component.ts

--- src/app/dialog-add-player/dialog-add-player.component.ts
+++ src/app/dialog-add-player/dialog-add-player.component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MatFormField } from '@angular/material/form-field';
+import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
 import { MatLabel } from '@angular/material/form-field';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-dialog-add-player',
   standalone: true,
-  imports: [MatFormField, MatLabel, FormsModule],
+  imports: [MatFormField, MatButtonModule, MatLabel, FormsModule, MatDialogModule, MatFormFieldModule, MatInputModule, FormsModule],
   templateUrl: './dialog-add-player.component.html',
   styleUrl: './dialog-add-player.component.scss'
 })
 export class DialogAddPlayerComponent {
+  name: string = '';
 
+  readonly dialogRef = inject(MatDialogRef<DialogAddPlayerComponent>);
+
+  onNoClick(): void {
+    this.dialogRef.close(null);
+  }
 }
